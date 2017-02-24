dc7f77698f8257b3f300f72d2c8b969bd6940028
Walker Lyle <Walker Lyle>
2017-02-24 16:21:12 -0600

    Create first react app

---
+++ first-reaction/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = function() {
+  return (
+    <h1>Hello React!</h1>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
